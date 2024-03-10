acccb54c4c908e96785c3b1c63a88b0fcb94d725
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2024-03-10 20:08:46 +0000

    feat: add astro lsp

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  ---@type TSConfig
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    ensure_installed = {
+      "astro",
+    },
+  },
+}
