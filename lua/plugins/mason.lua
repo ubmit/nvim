8b0c13a8b48698f64b03747b2355b6cdf23b2b3e
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2024-03-11 01:28:03 +0000

    refactor: manage lsps through mason

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "astro-language-server",
+      "css-lsp",
+      "emmet-language-server",
+      "svelte-language-server",
+      "tailwindcss-language-server",
+      "typescript-language-server",
+    },
+  },
+}
